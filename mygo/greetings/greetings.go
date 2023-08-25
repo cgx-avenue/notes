4c8685275a4f7ba2aceeec80f2fa7396a5b7e4b4
cgx-avenue <zuomujie_2019@qq.com>
2023-08-25 13:34:38 +0800

    vault backup: 2023-08-25 13:34:38

--- mygo/greetings/greetings.go
+++ mygo/greetings/greetings.go
@@ -5,12 +5,12 @@ import (
 )
 
 func Hello(name string) (string,error) {
-	if name == ""{
-		reutrn "",errors.New("empty name")
+	if name == "" {
+		return "", errors.New("empty name")
 	}
 
 	message := fmt.Sprintf("Hi, %v. Welcome!",name)
-	return message
+	return message,nil
 
 
 }
