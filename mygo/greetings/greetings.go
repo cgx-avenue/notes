3913b559e5d94b297af0b39f6a052710e29dd425
cgx-avenue <zuomujie_2019@qq.com>
2023-08-25 13:28:38 +0800

    vault backup: 2023-08-25 13:28:38

--- mygo/greetings/greetings.go
+++ mygo/greetings/greetings.go
@@ -1,6 +1,14 @@
 package greetings
-import "fmt"
-func Hello(name string) string {
+import (
+	"fmt"
+	"errors"
+)
+
+func Hello(name string) (string,error) {
+	if name == ""{
+		reutrn "",errors.New("empty name")
+	}
+
 	message := fmt.Sprintf("Hi, %v. Welcome!",name)
 	return message
 
