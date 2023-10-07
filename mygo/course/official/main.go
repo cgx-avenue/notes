1a9204d5e0e8bc4c2a18e6be654d1ef6809b4216
cgx-avenue <zuomujie_2019@qq.com>
2023-10-07 16:25:41 +0800

    vault backup: 2023-10-07 16:25:40

--- mygo/course/official/main.go
+++ mygo/course/official/main.go
@@ -1,20 +1,25 @@
 package main
 
 import (
-	
 	"fmt"
 	"net/http"
 )
 
-func myWeb(w http.ResponseWriter, r *http.Request){
+func myWeb(w http.ResponseWriter, r *http.Request) {
 	r.ParseForm()
-	for k,v :=
+	for k, v := range r.URL.Query() {
+		fmt.Println("key:", k, ",value:", v[0])
+	}
+	for k, v := range r.PostForm {
+		fmt.Println("key:", k, ",value:", v[0])
+	}
+	fmt.Fprintln(w, "this is a beginning")
 }
 
-func main(){
-	http.HandleFunc("/",myWeb)
+func main() {
+	http.HandleFunc("/", myWeb)
 	fmt.Println("server is starting, visit: http://localhost:8080")
-	err := http.ListenAndServe(":8080",nil)
+	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		fmt.Println("server started with error: ", err)
 	}
