42e69789f7f6f6d5c5c664da2b1bff9cd94f421a
cgx-avenue <zuomujie_2019@qq.com>
2023-10-07 15:49:41 +0800

    vault backup: 2023-10-07 15:49:40

--- mygo/course/official/main.go
+++ mygo/course/official/main.go
@@ -1,2 +1,20 @@
 package main
-import 
+
+import (
+	
+	"fmt"
+	"net/http"
+)
+
+func myWeb(w http.ResponseWriter, r *http.Request){
+	fmt.Fprintf(w,"this is a beginning")
+}
+
+func main(){
+	http.HandleFunc("/",myWeb)
+	fmt.Println("server is starting, visit: http://localhost:8080")
+	err := http.ListenAndServe(":8080",nil)
+	if err != nil {
+		fmt.Println("server started with error: ", err)
+	}
+}
